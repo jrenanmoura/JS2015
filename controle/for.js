31596181a811181b9dd12399e333fdd3ce532e4c
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-08-31 17:42:31 -0300

    estruras de controle for

---
+++ controle/for.js
@@ -0,0 +1,14 @@
+let contador = 1
+while (contador <=10){
+    console.log(`Numero ${contador}`)
+    contador++
+}
+
+for (let r = 1; r <= 10; r++) {
+    console.log(`i mostra ${r}`)
+}
+
+const notas = [6.6, 7.7, 8.8, 9.9, 2,2]
+for(let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`) // colchetes por ser um array
+}
