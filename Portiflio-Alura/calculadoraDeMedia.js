d893a31c89b6c82831dc639699492e5b2bf8c3f5
Renan <106356679+jrenanmoura@users.noreply.github.com>
2022-09-05 21:45:59 -0300

    Portifolio Calculadora

---
+++ Portiflio-Alura/calculadoraDeMedia.js
@@ -0,0 +1,22 @@
+var nome = "Renan"
+console.log(nome)
+
+var notaPrimeiroBi = 10
+var notaSegundoBi = 8
+var notaTerceiroBi = 6
+var notaQuartoBi = 5
+var nota1 = (notaPrimeiroBi+notaSegundoBi)
+var nota2 = (notaTerceiroBi+notaQuartoBi)
+
+var notaFinal = ((nota1 + nota2) /4)
+
+  const reprovaOuAprova = (notaFinal)
+  if (notaFinal >= 5.5){
+    console.log("Você foi Aprovado")
+  }else{
+        coonsole.log("Você foi Reprovado")
+    
+  }
+  console.log('Sua nota é '+ notaFinal.toFixed(1))
+// Revisão da 1 aula
+//Variaveis, string, toFixed, concatenação, console.log, operações matemáticas, console
